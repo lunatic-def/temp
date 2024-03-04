554829d4abdcc58e818d848d2a245779b1372a73
lunatic-def <111355545+lunatic-def@users.noreply.github.com>
2024-03-04 07:56:05 +0700

    Add files via upload

---
+++ test_sql_connect.php
@@ -0,0 +1,23 @@
+<?php
+define('DB_SERVER', getenv('DB_SERVER'));
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_DATABASE', getenv('DB_DATABASE'));
+echo DB_SERVER;
+echo "\n";
+echo DB_DATABASE;
+echo "\n";
+echo DB_USERNAME;
+echo "\n";
+echo DB_PASSWORD;
+echo "\n";
+$connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+
+if (!$connection) {
+    echo "false connection";
+    exit;
+}
+
+// Use the $connection object to interact with the database
+echo "Connection successful!";
+?>
